9fa737175637fbd9ec92e2bf804e5db7d07c718e
Nicole260205 <nicolecoelho26@gmail.com>
2025-08-04 15:18:29 -0300

    terminei a home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,44 @@
 import paper from "../assets/paper-tear.png";
+import logo from "../assets/logo.png"; // substitua se for outro nome
+import { FaInstagram, FaFacebookF, FaTwitter, FaYoutube } from "react-icons/fa";
 import "../styles/footer.css";
+
 function Footer() {
   return (
     <>
-      
       <div className="footer">
-        <h1>testeee</h1>
+        <img src={paper} alt="papel rasgado" className="paper-tear-footer" />
+
+        <div className="footer-content">
+          {/* Coluna 1 */}
+          <div className="footer-column left">
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">Cookies</a>
+          </div>
+
+          {/* Coluna 2 */}
+          <div className="footer-column center">
+            <img src={logo} alt="Logo" className="footer-logo" />
+            <p className="rights">© 2025 Todos os direitos reservados</p>
+          </div>
+
+          {/* Coluna 3 */}
+          <div className="footer-column right">
+            <a href="#">
+              <FaInstagram className="social-icon" />
+            </a>
+            <a href="#">
+              <FaFacebookF className="social-icon" />
+            </a>
+            <a href="#">
+              <FaTwitter className="social-icon" />
+            </a>
+            <a href="#">
+              <FaYoutube className="social-icon" />
+            </a>
+          </div>
+        </div>
       </div>
     </>
   );
